e8a3af6a7a5fd4e56ae1614e2ba4da6e022a038c
milkperson <36242229+milkperson@users.noreply.github.com>
2022-11-03 10:11:35 -0500

    remove unneeded register events

--- main.ts
+++ main.ts
@@ -92,15 +92,6 @@ export default class QuranLookupPlugin extends Plugin {
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new QuranLookupSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
 	onunload() {
